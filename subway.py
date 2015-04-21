e7b8a6a48c9463d8536da2eb8896cbf5eeeb05fe
Zach Donovan <zach@zachdonovan.net>
2015-04-21 18:07:27 -0400

    clean up subway.py

--- subway.py
+++ subway.py
@@ -17,6 +17,7 @@ class SubwaySystem(object):
           self._add_station(stop, neighbors)
 
     def take_train(self, **kwargs):
+        # this nonsense is required to support from as a keyword argument
         assert kwargs.get('from', False), "take_train requires a 'from' keyword argument"
         assert kwargs.get('to', False), "take_train requires a 'to' keyword argument"
         origin = kwargs['from']
@@ -28,29 +29,30 @@ class SubwaySystem(object):
         assert start_station, "take_train requires an origin point within the existing subway system"
         assert end_station, "take_train requires an end point within the existing subway system"
 
-        queue = [start_station]
-        paths = { origin: [] }
-        discovered = { origin: True }
-        while len(queue):
-            next_station = queue.pop()
-            next_station_name = next_station.get_name()
-            print("next station is {}".format(next_station))
+
+        # A Basic Bread-First Search
+
+        queue = [start_station]                                               # prime the queue with the first station
+        paths = { origin: [] }                                                # insert the start station into the list of candidate paths
+        discovered = { origin: True }                                         # mark the start station as discovered
+
+        while len(queue):                                                     # while we have unvisited stations
+            next_station = queue.pop()                                        # visit the next station
+            next_station_name = next_station.get_name()                       # extract the station's unique identifier
 
             discovered[next_station_name] = True                              # don't visit this station again
             path_to_station = paths[next_station_name] + [next_station_name]  # add this station to the path for all subsequent stations
-            if next_station == end_station:
-                print("we're done here")                
-                return path_to_station # current station is our destination
+            if next_station == end_station:                                   # if we're at the destination
+                return path_to_station                                        # return path to this station
 
-            for tup in next_station.get_neighbors():
+            for tup in next_station.get_neighbors():                          # get adjacent stations for exploration
                 neighbor, distance = tup
-                if discovered.get(neighbor, False):
-                    continue # we've already visited this station 
-                paths[neighbor] = path_to_station
-                queue.append(self._stations[neighbor])
-                print("queue is now {}".format(queue))
+                if discovered.get(neighbor, False):                           # unless we've already visited them
+                    continue
+                paths[neighbor] = path_to_station                             # set their paths to match the current station
+                queue.append(self._stations[neighbor])                        # enqueue!
 
-        return []
+        return []                                                             # fail case - no route found
 
 
     def _add_station(self, station_name, neighbors):
